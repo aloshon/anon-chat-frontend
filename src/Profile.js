45037b97c40bfd981b9e37039a08e8ab2b0b005f
Allen Loshon <allenlosho913@gmail.com>
2021-05-11 17:45:23 -0700

    allow user to delete profile

--- src/Profile.js
+++ src/Profile.js
@@ -3,31 +3,56 @@ import UserContext from "./UserContext";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import {useSelector, shallowEqual, useDispatch} from "react-redux";
-import {FETCH_BLOCK_LIST} from "./actionTypes";
 import BlockedUser from "./BlockedUser";
 import AddUserToBlockListForm from "./Forms/AddUserToBlockListForm";
-import {toggleDarkMode} from "./actionCreators";
+import {toggleDarkMode} from "./Actions/actionCreators";
+import AnonChatApi from "./api";
+import {useHistory} from "react-router-dom";
+import {getBlockList} from './Actions/actionCreators';
 
 const Profile = () => {
-    const {user} = useContext(UserContext);
+    const {user, logout} = useContext(UserContext);
     const dispatch = useDispatch();
+    const history = useHistory();
     const blockList = useSelector(state => state.blockList, shallowEqual);
+    const darkMode = useSelector(state => state.darkMode);
    
     // Get the user's blockList on render after user is defined
     useEffect(() =>{
         if(user){
-            dispatch({type: FETCH_BLOCK_LIST, payload: user.blockList});
+            getBlockList(dispatch, user.blockList);
         }
     }, [user, dispatch]);
+
+    const handleDelete = () => {
+        if(window.confirm("Delete your profile?")){
+            logout();
+            AnonChatApi.deleteUser();
+            history.push('/signup')
+        } else{
+            return
+        }
+    }
 
     while(!user) return <h1>Loading...</h1>;
 
     return(
         <Container>
             <h1>Username: {user.username}</h1><br/>
-            <Button onClick={() => toggleDarkMode(dispatch)}>toggle dark mode</Button>
+            <div className='custom-control custom-switch'>
+                <input
+                type='checkbox'
+                className='custom-control-input'
+                id='customSwitchesChecked'
+                onChange={() => toggleDarkMode(dispatch)}
+                checked={darkMode.isDarkMode ? true : false}
+                />
+                <label className='custom-control-label' htmlFor='customSwitchesChecked'>
+                {darkMode.isDarkMode ? "dark mode" : "light mode"}
+                </label>
+            </div>
             <AddUserToBlockListForm />
-            <h5>Block List</h5>
+            <h5 style={{marginTop: "90px"}}>Block List</h5>
             <Container>
                 {blockList.length !== 0 ? blockList.map(b => (
                     <BlockedUser 
@@ -36,6 +61,12 @@ const Profile = () => {
                     />
                 )) : <h2>No users blocked</h2>}
             </Container>
+            <Button
+            style={{ float: "left", marginTop: "30px" }}
+            variant="danger" 
+            onClick={handleDelete}>
+                Delete Profile
+            </Button>
         </Container>
     )
 }
